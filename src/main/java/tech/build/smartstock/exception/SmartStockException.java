44199e95b3807c6484155c7dfe4686b2d564a4c7
Iago Carvalho <iagocarvalhos10@gmail.com>
2025-09-04 10:38:43 -0300

    Exceptions

---
+++ src/main/java/tech/build/smartstock/exception/SmartStockException.java
@@ -0,0 +1,8 @@
+package tech.build.smartstock.exception;
+
+public class SmartStockException extends RuntimeException {
+
+    public SmartStockException(String message) {
+        super(message);
+    }
+}
